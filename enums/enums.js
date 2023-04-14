2fdf839055225eacbd16bdb6d7f41be8a7c38867
Bohdan Degtiar <bohdan.degtiar@binary-studio.com>
2023-04-14 10:05:40 +0300

    c-4: + add cassandra and json schema types enum

---
+++ enums/enums.js
@@ -0,0 +1,4 @@
+const { JsonSchemaType } = require("./json-schema-type");
+const { CassandraType } = require("./cassandra-data-type");
+
+module.exports = { JsonSchemaType, CassandraType };
---
+++ enums/json-schema-type.js
@@ -0,0 +1,11 @@
+const JsonSchemaType = {
+  STRING: "string",
+  NUMBER: "number",
+  INTEGER: "integer",
+  OBJECT: "object",
+  ARRAY: "array",
+  BOOLEAN: "boolean",
+  NULL: "null",
+};
+
+module.exports = { JsonSchemaType };
